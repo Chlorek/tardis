d52dc16d53f6e06aa281ca280100f96633dc5589
chlorek <chlorek.cl@gmail.com>
2017-10-28 14:53:14 +0200

    using lambda as check expression instead of values

--- noise_atlas.hpp
+++ noise_atlas.hpp
@@ -18,14 +18,21 @@
 #include "noise_source.hpp"
 #include <utility>
 #include <cstddef>
-#include <cstring>
-#include <limits>
-#include <cassert>
 #include <memory>
+#include <vector>
+#include <functional>
 
-namespace tardis {
+namespace tardis {  
     template<typename T>
     class noise_atlas : public noise_source {
+        private:
+            struct dict {
+                dict(const std::function<bool(int32_t)>& check_func, std::shared_ptr<noise_source> gen) :
+                check_func(check_func), gen(gen) {};
+                
+                const std::function<bool(int32_t)> check_func;
+                std::shared_ptr<noise_source> gen;
+            };
         public:
             noise_atlas(T atlas_src) {
                 this->atlas_src = std::make_shared<T>(atlas_src);
@@ -33,56 +40,34 @@ namespace tardis {
             
             noise_atlas(const noise_atlas& orig) {
                 atlas_src = orig.atlas_src;
-                sources_size = orig.sources_size;
-                highest_src = orig.highest_src;
-                delete[] sources;
-                sources = new std::shared_ptr<noise_source>[sources_size];
-                for(std::size_t i = 0; i < sources_size; ++i)
-                    sources[i] = orig.sources[i];
+                atlas_dict = orig.atlas_dict;
             }
             
             virtual ~noise_atlas() {
-                delete[] sources;
+                //..
             }
             
-            /* bind_val must be non-negative value */
-            template<typename U>
-            void bind(const int32_t bind_val, U gen) {
-                assert(bind_val >= 0);
-                if(bind_val > highest_src || sources_size == 0) {
-                    std::shared_ptr<noise_source>* newSources = new std::shared_ptr<noise_source>[bind_val+1];
-
-                    // copy existing elements
-                    for(uint32_t i = 0; i < sources_size; ++i)
-                        newSources[i] = sources[i];
-                    // fill missing with nullptr
-                    for(int32_t i = sources_size; i < bind_val; ++i)
-                        newSources[i] = nullptr;
-                    // finally add new value
-                    newSources[bind_val] = std::make_shared<U>(gen);
-                    
-                    delete[] sources;
-                    sources = newSources;
-                    
-                    highest_src = bind_val;
-                    sources_size = bind_val+1;
-                }
-                else {
-                    sources[bind_val] = std::make_shared<U>(gen);
-                }
+            template<typename NS>
+            void bind(const std::function<bool(int32_t)>& cf, NS gen) {
+                atlas_dict.push_back(dict(cf, std::make_shared<NS>(gen)));
+            }
+            
+            template<typename NS>
+            void bind(int32_t val, NS gen) {
+                atlas_dict.push_back(dict([val](int32_t v) {return v==val;}, std::make_shared<NS>(gen)));
             }
             
             int32_t at(int16_t x, int16_t y) {
-                auto h = atlas_src->at(x, y);
-                if(h <= highest_src && sources[h] != nullptr)
-                    return sources[h]->at(x, y);
+                auto v = atlas_src->at(x, y);
+                for(dict check : atlas_dict) {
+                    if(check.check_func(v))
+                        return check.gen->at(x, y);
+                }
                 return 0;
             }
         private:
             std::shared_ptr<T> atlas_src;
-            std::shared_ptr<noise_source>* sources = new std::shared_ptr<noise_source>[0];
-            std::size_t sources_size = 0;
-            int32_t highest_src = 0;
+            std::vector<dict> atlas_dict;
     };
 }
 
